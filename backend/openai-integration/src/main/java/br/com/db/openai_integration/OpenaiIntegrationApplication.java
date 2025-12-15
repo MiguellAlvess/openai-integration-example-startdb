763765a27c8fec93f52d18a6c7dbc7f5eb043aec
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-12-15 17:57:32 -0300

    chore: add initial project setup

---
+++ backend/openai-integration/src/main/java/br/com/db/openai_integration/OpenaiIntegrationApplication.java
@@ -0,0 +1,13 @@
+package br.com.db.openai_integration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenaiIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenaiIntegrationApplication.class, args);
+	}
+
+}
---
+++ backend/openai-integration/src/test/java/br/com/db/openai_integration/OpenaiIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.db.openai_integration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenaiIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
