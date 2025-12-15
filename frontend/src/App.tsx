ed0f7a30d7832bf7ae42b713f90c1652e0530eaa
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-12-15 20:01:20 -0300

    chore: add shadcn setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,11 @@
 const VirtualAssistantPage = () => {
   return (
     <div>
-      <h1>Virtual Assistant</h1>
+      <h1 className="justify-normal font-bold text-red-300">
+        Virtual Assistant
+      </h1>
     </div>
-  );
-};
+  )
+}
 
-export default VirtualAssistantPage;
+export default VirtualAssistantPage
